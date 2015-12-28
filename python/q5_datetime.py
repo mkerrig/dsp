ee20c8cb4fda8d622dfd19908c85070eb450d0f4
mkerrig <matt@networkinguys.com>
2015-12-27 22:13:32 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,23 @@
-# Hint:  use Google to find python function
+from datetime import datetime
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+def daysdiff(formt,datestrt,datestp):
+   d1 = datetime.strptime(datestrt, formt)
+   d2 = datetime.strptime(datestp, formt)
+   return (d2-d1).days
+###a
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+fmt = '%m-%d-%Y'
+print "A: " ,daysdiff(fmt,date_start,date_stop),"\n"
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+###b
+date_start = '12312013'
+date_stop = '05282015'
+fmt = '%m%d%Y'
+print "B: " ,daysdiff(fmt,date_start,date_stop),"\n"
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+###c
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+fmt = '%d-%b-%Y'
+print "C: " ,daysdiff(fmt,date_start,date_stop),"\n"
