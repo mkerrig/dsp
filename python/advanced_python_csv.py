9729481dd443f7c527c0acfa8123876de74993f2
mkerrig <matt@networkinguys.com>
2016-01-10 01:44:55 -0500

    Update advanced_python_csv.py
    @klq

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,23 @@
-PLACE YOUR CODE HERE
+import csv
+
+def read_data():
+  email = []
+  with open('faculty.csv','rb') as f:
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+      if index ==0:
+        cat = row
+        index = 1
+      else:
+        email.append(row[cat.index(' email')])
+  return email
+
+def write_data(list):
+  with open('emails.csv','wb') as f:
+    write = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    write.writerow(['Email'])
+    for email in list:
+      write.writerow([email])
+
+write_data(read_data())
