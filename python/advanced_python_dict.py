2f6797204142d59af1ad73605d9aa054faf2fdbe
mkerrig <matt@networkinguys.com>
2016-01-10 04:26:47 -0500

    Update advanced_python_dict.py
    @klq

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,73 @@
-PLACE YOUR CODE HERE
+import csv
+import collections
+
+def question_6():
+  faculty = {}
+  with open('faculty.csv','rb') as f:
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+      if index ==0:
+        cat = row
+        index = 1
+      else:
+        linename = row[cat.index('name')].split()
+        degree = row[cat.index(' degree')]
+        title = row[cat.index(' title')].replace(" is ", " of ")
+        email = row[cat.index(' email')]
+        templist = [degree,title[0:title.index(" of")],email]
+        if linename[-1] in faculty:
+          faculty[linename[-1]].append(templist)
+        else:
+          faculty[linename[-1]] =[]
+          faculty[linename[-1]].append(templist)
+    print faculty
+
+question_6()
+
+def question_7():
+  faculty = {}
+  with open('faculty.csv','rb') as f:
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+      if index ==0:
+        cat = row
+        index = 1
+      else:
+        linename = row[cat.index('name')].split()
+        degree = row[cat.index(' degree')]
+        title = row[cat.index(' title')].replace(" is ", " of ")
+        email = row[cat.index(' email')]
+        templist = [degree,title[0:title.index(" of")],email]
+        if len(linename[0]) < 3:
+          faculty[(linename[1],linename[-1])] = templist
+        else:
+          faculty[(linename[0],linename[-1])] = templist
+    print faculty
+question_7()
+
+def question_8():
+  faculty = {}
+  with open('faculty.csv','rb') as f:
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+      if index ==0:
+        cat = row
+        index = 1
+      else:
+        linename = row[cat.index('name')].split()
+        degree = row[cat.index(' degree')]
+        title = row[cat.index(' title')].replace(" is ", " of ")
+        email = row[cat.index(' email')]
+        templist = [degree,title[0:title.index(" of")],email]
+        if len(linename[0]) < 3:
+          faculty[(linename[1],linename[-1])] = templist
+        else:
+          faculty[(linename[0],linename[-1])] = templist
+    getkey = lambda item: item[0][1]
+    facultynew = collections.OrderedDict(sorted(faculty.items(),key=getkey))
+    print facultynew
+
+question_8()
