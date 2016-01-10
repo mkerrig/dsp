815ab3c276fe880cda549367e64a3dab6d8b34da
mkerrig <matt@networkinguys.com>
2016-01-10 01:13:56 -0500

    Update advanced_python_regex.py
    @klq

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,32 @@
-PLACE YOUR CODE HERE
+
+import csv
+
+def read_data():
+  degrees = {}
+  title = {}
+  email = []
+  with open('faculty.csv','rb') as f:
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+      if index ==0:
+        cat = row
+      else:
+        linedeg = row[cat.index(' degree')].split()
+        for deg in linedeg:
+          if deg.translate(None, '.') in degrees:
+            degrees[deg.translate(None, '.')] += 1
+          elif deg != '0':
+            degrees[deg.translate(None, '.')] = 1
+
+        if row[cat.index(' title')].replace(" is ", " of ") in title:
+          title[row[cat.index(' title')].replace(" is ", " of ")] += 1
+        else:
+          title[row[cat.index(' title')].replace(" is ", " of ")] = 1
+        email.append(row[cat.index(' email')])
+      index+=1
+    print degrees,'\n'
+    print title, '\n'
+    print email,'\n'
+
+read_data()
